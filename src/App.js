9bdec43e2f07ac5a25129b4e903344b1b041271a
marlon <marlon.savian@gmail.com>
2020-06-26 16:29:25 -0300

    s

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import Home from './pages/Home';
 import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, Router} from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Home></Home>
-      <Rooms></Rooms>
-      <SingleRoom></SingleRoom>
-      <Error></Error>
+    <Switch>
+
+    
+    <Route exact path="/" component={Home}/>
+    <Route exact path="/rooms" component={Rooms}/>
+    <Route exact path="/rooms/:id" component={SingleRoom}/>
+    <Route component={Error}/>
+    </Switch>  
     </>
 
   );
